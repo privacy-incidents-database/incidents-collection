c8842c4b24ea391179a1fcd7f98b8a515d90d1e3
Sarvesh <sarveshr7@gmail.com>
2016-01-22 02:03:05 -0500

    Added query keywords as arguments, changed filenames to headline names, getting content using headline name

--- collect.py
+++ collect.py
@@ -2,42 +2,83 @@ import json
 import requests
 import os
 import sys
+import urllib2
+# use this package to get rid of the html and js tags.
+import getopt
+from bs4 import BeautifulSoup
+
+
 def collect():
-    import urllib2
-    API_KEY=os.environ.get("NY_TIMES_API_KEY")
-    url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=privacy+leakage&page=0&fl=abstract,byline,lead_paragraph,web_url,word_count&api-key=" + API_KEY
+
+    keywordstr = getkeywords()
+
+    API_KEY = os.environ.get("NY_TIMES_API_KEY")
+    url = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + keywordstr + \
+    "&page=0&fl=abstract,byline,headline,web_url,word_count&api-key=" + API_KEY
     r = requests.get(url)
-    if r.status_code == 200 :
+    if r.status_code == 200:
         w = r.json()
         if w.has_key('response'):
-            docs =  w['response']['docs']
-            i = 0
+            hits = w['response']['meta']['hits']
+            print "Number of articles for the current Query = " + str(hits)
+            docs = w['response']['docs']
+
             for d in docs:
-                if d['web_url'] is not None and d['lead_paragraph'] is not None:
+                if d['web_url'] is not None and d['headline']['main'] is not None:
+
+                    #Extract the file name from the url
+                    filename = d['web_url'].rsplit('/',1)[1]
+                    filename = filename.rsplit('.',1)[0]
+
                     try:
-                        # use urllib2 to read the html content instead of using wget to fetch local
+                        # use urllib2 to read the html content instead of using
+                        # wget to fetch local
                         response = urllib2.urlopen(d['web_url'])
                         html = response.read()
-                        clean(html,d['lead_paragraph'],i)
-                        i+=1#indexing
-                    except Exception,e:
+                        clean(html, d['headline']['main'], filename)
+
+
+                    except Exception, e:
                         print e
                         print "not valid"
-                
-                        
+
         print r
-def clean(string,lead_para,index):
-    def clean_html( fragment ):
-        from bs4 import BeautifulSoup#use this package to get rid of the html and js tags.
-        soup = BeautifulSoup(fragment, 'html.parser')
-        return soup.get_text()
-    remove_tag = clean_html(string)#remove tags
-    start = remove_tag.find(lead_para[0:50])#find start point according to lead_paragraph
-    remove_start = remove_tag[start:]#start from the lead para_graph
-    end = remove_start.find("\n\n\n\n")#mark the end using 4 continuous endline
-    final = remove_start[:end]#final ver
-    wr = open("webpage/%s.txt" % index,'w')#write into txt file.
-    wr.write(final+"\n")
+
+
+def clean(string, headline, filename):
+    remove_tag = clean_html(string)  # remove tags
+    whtml = open("html/%s.txt" % filename, 'w')
+    whtml.write(remove_tag.encode('utf-8') + "\n")
+    # find start point according to lead_paragraph
+    start = remove_tag.find(headline,0)
+    start = start + len(headline)
+    start = remove_tag.find(headline, start)
+    remove_start = remove_tag[start:]  # start from the lead para_graph
+    # mark the end using string Inside NYTimes.com
+    end = remove_start.find("Inside NYTimes.com")
+
+    final = remove_start[:end].rstrip()  # final ver
+
+    wr = open("webpage/%s.txt" % filename, 'w')  # write into txt file.
+    wr.write(final.encode('utf-8') + "\n")
     return final
-    
+
+
+def clean_html(fragment):
+    soup = BeautifulSoup(fragment, 'html.parser')
+    return soup.get_text()
+
+def getkeywords():
+    keywords = sys.argv[1:]
+    keywordstr = ""
+    cnt = 0
+
+    for keyword in keywords:
+        keywordstr += keyword
+        if cnt < len(keywords) - 1 :
+            keywordstr += "+"
+        cnt += 1
+
+    return keywordstr
+
 collect()
