d98fed35806fd4cea2c4918d433668af8f65bdfa
Jesse Xu <jesse.xu.20@gmail.com>
2016-02-10 14:13:47 -0500

    add keywords to column

--- gen_csv.py
+++ gen_csv.py
@@ -8,8 +8,20 @@ def gen_csv(neg,pos):
     """
     fout = open('data.csv','w')
     out = csv.writer(fout)
-    dic = ['no.','type_of_material','news_desk','word_count','keywords','document_type','pub_date','byline','isPrivacy'];
     src = [neg,pos]
+    dic = ['no.','type_of_material','news_desk','word_count','document_type','pub_date','byline','isPrivacy'];
+    length = len(dic)
+    keywords = []
+    
+    for s in src:
+        for filename in os.listdir(s):
+            fin = open(s+'/'+filename)
+            dat = json.load(fin)
+            if dat.has_key('keywords'):
+                for st in dat['keywords']:
+                    keywords.append(st['value'])
+    rank = [0]*(len(keywords))
+    dic.extend(sorted(keywords))
     out.writerow(dic)
     cnt = 1
     for s in src:
@@ -18,7 +30,8 @@ def gen_csv(neg,pos):
             ###filename => every file in the directory
             fin = open(s+'/'+filename)
             dat = json.load(fin)
-            for i in range(1,len(dic)-1):
+            for i in range(1,length-1):
+                
                 try:
                     #valid json, key
                     dat[dic[i]]
@@ -28,11 +41,6 @@ def gen_csv(neg,pos):
                     dat[dic[i]]='none'
                 finally:
                     ###list item
-                    if dic[i] == 'keywords':
-                        string = ''
-                        for st in dat['keywords']:
-                            string = string + st['value'] + ','
-                        dat['keywords'] = string[0:len(string)-1]
                     if dic[i] == 'byline':
                         string = ''
                         people =  dat['byline']['person']
@@ -53,6 +61,13 @@ def gen_csv(neg,pos):
                         #to ascii since there are some Spanish name
                         string.encode('ascii')
                         dat['byline'] = string[0:len(string)-1]
+                    #modify keywords
+                    if dat.has_key('keywords'):
+                        for st in dat['keywords']:
+                            if 'rank' in st:
+                                rank[keywords.index(st['value'])] = st['rank']
+                            else:
+                                rank[keywords.index(st['value'])] = 1
                     try:
                         dat[dic[i]] = str(dat[dic[i]])
                     except Exception, e:
@@ -60,7 +75,10 @@ def gen_csv(neg,pos):
             flag = False
             if s == pos:
                 flag = True
-            out.writerow([cnt,dat['type_of_material'].lower(),dat['news_desk'].lower(),dat['word_count'].lower(),dat['keywords'].lower(),dat['document_type'].lower(),dat['pub_date'].lower(),dat['byline'],flag])
+            result = [cnt,dat['type_of_material'].lower(),dat['news_desk'].lower(),dat['word_count'].lower(),dat['document_type'].lower(),dat['pub_date'].lower(),dat['byline'],flag]
+            result.extend(rank)
+            out.writerow(result)
+            rank =  [0]*(len(keywords))
             cnt+=1
             fin.close()
 gen_csv('dat/NYnegative/json','dat/NYpositive/json')
