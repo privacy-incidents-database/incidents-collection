cd968620cbc56f0c027a6742bc0815e485a75aa3
Jesse Xu <jesse.xu.20@gmail.com>
2016-03-03 23:32:01 -0500

    tfid

---
+++ NLP/tfid_log.py
@@ -0,0 +1,39 @@
+import csv,json,sys,os,math
+def gen_csv(neg,pos):
+    """
+    Generate CSV according to the json stored.
+    neg: path to negative directory
+    pos: path to postive directory
+    
+    """
+    fout = open('data.csv','w')
+    out = csv.writer(fout)
+    src = [neg,pos]
+    # length = len(dic)
+    keywords = {}
+    
+    ##Get all the keywords
+    for s in src:
+        for filename in os.listdir(s):
+            fin = open(s+'/'+filename)
+            dat = json.load(fin)
+            for key in dat:
+                if key in keywords:
+                    keywords[key] += 1
+                else:
+                    keywords[key] = 1
+    # dic.extend(sorted(keywords))
+    ##sorted and write as header
+    out.writerow(sorted(keywords.keys()))
+    val = []
+    for key in sorted(keywords.keys()):
+        val.append(keywords[key])
+    out.writerow(val)
+    log_val = []
+    length = len(val)
+    for v in val:
+        log_val.append(float(math.log10(length/v)))
+    out.writerow(log_val)
+    fin.close()
+gen_csv('tfreq/content','tfreq/NYnegative')
+    
